05f16a873adafb8b521cef0787f84338b0c5e5e0
marekgluza <marekgluza@gmail.com>
2021-11-19 10:46:36 +0800

    adding setuptools files for distributing package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='simple_exact_diagonalization_routines',
+      version='0.1',
+      description='A set of functions useful for quick exact diagonalization computations and checks.',
+      url='https://github.com/marekgluza/Gaussian_fermions.git',
+      author='Marek Gluza',
+      license='GPL-3.0',
+      packages=find_packages(),
+      zip_safe=False)
